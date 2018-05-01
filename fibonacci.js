3c293ea3777c860969a04c0c046fedc3b1663f8e
RICHARD C BERBERIAN <humblemind@hotmail.com>
2018-04-30 23:27:27 -0700

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,29 @@
+// Simple Recursion
+function fibonacci(n) {
+    if (n < 2) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+// Dynamic Programming: Memoization
+function fibonacci(n, memo = []) {
+    if (n < 2) return n;
+    if (memo[n]) return memo[n];
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+}
+
+// Dynamic Programming: Tabulation
+function fibonacci(n) {
+    if (n < 2) return n;
+    let i = 2;
+    let num1 = 1;
+    let num2 = 0;
+    let temp;
+    while (i < n) {
+        temp = num1 + num2;
+        num2 = num1;
+        num1 = temp;
+        i++;
+    }
+    return num1 + num2;
+}
